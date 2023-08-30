83a5205067ccba76c7442f3eb00e5ce76f38c35c
Chibuokem Ukandu <130072804+kems18@users.noreply.github.com>
2023-08-30 20:01:26 +0000

    Solution

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,15 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+
+    for(let i = 1; i <= destination; i++) {
+        sum +- i;
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -38,18 +42,22 @@ function countEvenNumbersWithin(destination) {
     let sum = 0;
     let count = 0;
     let arrayOfEvenNumbers = [];
+    for(let i = 1; i <= destination; i++) {
+        if( i % 2 === 0 ) {
+            sum += i;
+            count += 1;
+            arrayOfEvenNumbers.push(i)
+        }
+    }
 
     return {
-        // property value shorthand
-        // when the property name and the value name are the same
-        // you can just write the property name in your object
         count,
         sum,
         arrayOfEvenNumbers
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+ assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
